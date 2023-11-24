f199f757016d1b5e19fe2c3744017e0fde6e6d51
12v <11772383+12v@users.noreply.github.com>
2023-11-24 21:42:41 +0000

    Switch to using 'available' field

--- script.sh
+++ script.sh
@@ -8,20 +8,9 @@ RESPONSE=$(curl -s "https://bookings.drumshedslondon.com/api/4.0/package/ticketa
 
 echo $RESPONSE
 
-CALCULATED_HASH=$(echo -n $RESPONSE | sha256sum)
-
-echo $CALCULATED_HASH
-
-EXPECTED_HASH="d5d3327091ebff0ce9388371d38dd3ceedb995fec29a9052cbb6dc1d590e675e - "
-
-echo $EXPECTED_HASH
-
-CALCULATED_HASH=$(echo -n "$CALCULATED_HASH" | tr -d '[:space:]')
-EXPECTED_HASH=$(echo -n "$EXPECTED_HASH" | tr -d '[:space:]')
-
-if [ "$CALCULATED_HASH" = "$EXPECTED_HASH" ]; then
-echo "The strings are equal."
+if echo "$RESPONSE" | jq '.tickets[] | select(.available > 0)' | grep -q .; then
+    echo "There are tickets available."
+    exit 111
 else
-echo "The strings are not equal."
-exit 111
+    echo "No tickets available."
 fi
